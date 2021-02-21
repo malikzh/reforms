c117bcb7db1850f10f2759b87a72c065bf4bc9f8
Malik Zharykov <cmalikz.h@gmail.com>
2021-02-22 02:57:09 +0600

    fix

--- examples/InputsOutputs.vue
+++ examples/InputsOutputs.vue
@@ -5,8 +5,6 @@
   </div>
 </template>
 <script>
-import ReformsInput from "../src/ReformsInput";
-import ReformsOutput from "../src/ReformsOutput";
 import StringType from "./types/StringType";
 export default {
   name: "InputsOutputs",
