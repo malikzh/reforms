c3bbd2751b118f1daa5f7935da875ae06cea8044
Malik Zharykov <cmalikz.h@gmail.com>
2021-03-05 15:58:32 +0600

    email rule

--- src/lang/ru.js
+++ src/lang/ru.js
@@ -7,6 +7,9 @@ export default {
         string: {
             must_be_string: 'Значение должно быть строкой',
             is_string: 'Корректно заполнено'
+        },
+        email: {
+            must_be_email: 'Указан некоректный E-mail'
         }
     },
 };
--- src/validator/StringValidator.js
+++ src/validator/StringValidator.js
@@ -27,5 +27,61 @@ export default {
         }
 
         return result;
-    }
+    },
+    email(params) {
+        // got from: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+        const rem = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        const lang = params.lang.validation.email;
+        const values = !params.multiple ? [params.value] : params.value;
+
+        let result = [];
+
+        for (const value of values) {
+            if (_.isNil(value)) {
+                result.push({
+                    isValid: true,
+                    messages: [],
+                });
+                continue;
+            }
+
+            const isEmail = rem.test(String(value).toLowerCase());
+
+            result.push({
+                isValid: isEmail,
+                messages: isEmail ? [] : [lang.must_be_email],
+            });
+        }
+
+        return result;
+    },
+    url(params) {
+        //
+    },
+    'in'(params, notMode) {
+        //
+    },
+    not_in(params) {
+        return this['in'](params, true);
+    },
+    regex(params) {
+        //
+    },
+    starts_with(params) {
+        //
+    },
+    ends_with(params) {
+        //
+    },
+    contains(params) {
+        //
+    },
+    alpha(params) {
+        //
+    },
+    alphanum(params) {
+        //
+    },
+
 };
